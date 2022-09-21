098d05e8457f1ac2595b5dc3981be6299d4df6b0
liam-ob <liam.obrien@atteris.com>
2022-09-21 09:52:26 +0800

    updating Project

--- first_project.py
+++ first_project.py
@@ -1,27 +1,8 @@
-from dash import Dash
-from dash import html
-from dash import dcc
-import plotly.express as px
+def main():
+    x, y = 5, 10
+    z = x + y
+    print(f'{y} + {x} = {z}')
 
-app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-
-# get australias dataframe from plotly expresses dataset api
-data_frame = px.data.gapminder().query("country=='Australia'")
-
-# create a plotly line graph object with the dataframe
-fig = px.line(data_frame, x="year", y="pop", color="country", title="Title")
-
-app.layout = html.Div(children=[
-    # display html h1 component
-    html.H1(children='Australia Population Growth'),
-
-    # display dash core component as a plotly graph object
-    dcc.Graph(
-        id='example-graph', figure=fig)
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    main()
